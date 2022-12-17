0c2bf9645cff5759eca46a26db5be45951853ded
valihan <ilyasovvalihan@gmail.com>
2022-12-17 22:52:59 +0300

    fix function

--- main.go
+++ main.go
@@ -19,45 +19,91 @@ type Movie struct {
 var allMovie []Movie
 
 func getAllMovie(w http.ResponseWriter, r *http.Request) {
-	var movie Movie
-	err := json.NewDecoder(r.Body).Decode(&movie)
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(allMovie)
 	if err != nil {
-		log.Fatalln("There was an error decoding the request body into the struct")
+		log.Fatalln("There was an error in encoding ")
 	}
-	ur := r.URL
-	fmt.Println(ur)
-	fmt.Println(allMovie)
 }
 
 func getByIdMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
-	fmt.Println(vars["id"])
 	id := vars["id"]
+	var movieById *Movie
 	for i := 0; i < len(allMovie); i += 1 {
 		if allMovie[i].Id == id {
-			fmt.Println(allMovie[i])
+			movieById = &allMovie[i]
 			break
+		}
+	}
+	if movieById != nil {
+		err := json.NewEncoder(w).Encode(movieById)
+		if err != nil {
+			log.Fatalln("There was an error in encoding ")
 		}
 	}
 }
 
 func postMovie(w http.ResponseWriter, r *http.Request) {
-	var movie Movie
+	w.Header().Set("Content-Type", "application/json")
+	var movie *Movie
 	err := json.NewDecoder(r.Body).Decode(&movie)
 	if err != nil {
 		log.Fatalln("There was an error decoding the request body into the struct")
 	}
-	ur := r.URL
-	fmt.Println(ur)
-	allMovie = append(allMovie, movie)
-	fmt.Println(movie)
+	if movie != nil {
+		allMovie = append(allMovie, *movie)
+		err := json.NewEncoder(w).Encode(movie)
+		if err != nil {
+			log.Fatalln("There was an error in encoding ")
+		}
+	}
 }
 
 func putByIdMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var movie *Movie
+	err := json.NewDecoder(r.Body).Decode(&movie)
+	if err != nil {
+		log.Fatalln("There was an error decoding the request body into the struct")
+	}
+	var movieById *Movie
+	for i := 0; i < len(allMovie); i += 1 {
+		if allMovie[i].Id == id {
+			allMovie[i] = *movie
+			movieById = movie
+			break
+		}
+	}
+	if movieById != nil {
+		err := json.NewEncoder(w).Encode(movieById)
+		if err != nil {
+			log.Fatalln("There was an error in encoding ")
+		}
+	}
 }
 
 func deleteById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var movieById *Movie
+	for i := 0; i < len(allMovie); i += 1 {
+		if allMovie[i].Id == id {
+			movieById = &allMovie[i]
+			allMovie = append(allMovie[:i], allMovie[i+1:]...)
+			break
+		}
+	}
+	if movieById != nil {
+		err := json.NewEncoder(w).Encode(movieById)
+		if err != nil {
+			log.Fatalln("There was an error in encoding ")
+		}
+	}
 
 }
 
